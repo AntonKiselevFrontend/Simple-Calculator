b832cfadde30925b4f38a31176e30bce8587439c
Anton Kiselev <104986333+AntonKiselevFrontend@users.noreply.github.com>
2022-12-30 23:55:33 +0300

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,53 @@
+let calc = document.querySelector(".calc");
+let enter = document.querySelector("#enter");
+let result = document.querySelector("#total");
+
+calc.addEventListener("click", function (event) {
+    if(!event.target.classList.contains('calc__btn')) return;
+    let value = event.target.innerText
+    
+
+    switch (value) {
+        case "AC":
+            enter.textContent = "";
+            result.textContent = "";
+            break;
+        
+        case "=":
+            let equal = new Function ("return " + enter.textContent)();
+            result.textContent = equal;
+            if (result.textContent.length >= 9) {
+                result.textContent = Number(equal).toFixed(9);
+            }
+            enter.textContent = "";
+            break;
+
+        case "%":
+            result.textContent = new Function ("return " + enter.textContent)() / 100;
+            break;
+
+        case "( )":
+            enter.textContent = "(" + enter.textContent + ")";
+            break;
+
+        default:
+            enter.textContent += value
+    }
+
+    
+})
+
+let backspace = document.querySelector("#back");
+backspace.addEventListener("click", function () {
+    enter.textContent = String(enter.textContent).slice(0, -1)
+});
+
+let percentage = document.querySelector("#percent");
+percentage.addEventListener("click", function () {
+    result.textContent = enter.textContent / 100
+    if (result.textContent == "NaN") {
+        result.textContent = "Error"
+    } else {
+        result.textContent = enter.textContent / 100
+    }
+});
